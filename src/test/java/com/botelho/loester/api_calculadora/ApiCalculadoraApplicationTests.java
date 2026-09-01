a9191f0aec356615168c4bbe7f0e6fb4c8ca887e
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-08-31 21:31:36 -0300

    first commit

---
+++ src/test/java/com/botelho/loester/api_calculadora/ApiCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botelho.loester.api_calculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
