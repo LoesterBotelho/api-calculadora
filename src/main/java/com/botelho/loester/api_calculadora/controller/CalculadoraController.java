f520d38188bdb5e8ad9d8ff6f1f423b468da7ae2
Loester Botelho <156843598+LoesterBotelho@users.noreply.github.com>
2026-09-04 21:16:05 -0300

    ajuste de dependencias no pom.xml, ajuste no controle e service da calculadora.

--- src/main/java/com/botelho/loester/api_calculadora/controller/CalculadoraController.java
+++ src/main/java/com/botelho/loester/api_calculadora/controller/CalculadoraController.java
@@ -3,6 +3,7 @@ package com.botelho.loester.api_calculadora.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.botelho.loester.api_calculadora.dto.request.CalculadoraRequisicao;
@@ -10,28 +11,37 @@ import com.botelho.loester.api_calculadora.dto.response.CalculadoraResposta;
 import com.botelho.loester.api_calculadora.service.CalculadoraService;
 
 @RestController
+@RequestMapping("/calculadora")
 public class CalculadoraController {
-
-	@Autowired
-	CalculadoraService calculadoraService;
 	
-	@PostMapping("/calculadora/somar")
+	// deve ser sempre private final
+	private final CalculadoraService calculadoraService;
+
+	// melhor para testes junit 6 e mockito
+	// maneira correta de fazer	
+	// após a versão 3 do spring é opcional @Autowired
+	@Autowired
+	public CalculadoraController(CalculadoraService calculadoraService) {
+		this.calculadoraService = calculadoraService;
+	}
+
+	@PostMapping("/somar")
 	public CalculadoraResposta calculadoraSomar(@RequestBody CalculadoraRequisicao request) {
 		return calculadoraService.somar(request);
 	}
-	
-	@PostMapping("/calculadora/subtrair")
+
+	@PostMapping("/subtrair")
 	public CalculadoraResposta calculadoraSubtrair(@RequestBody CalculadoraRequisicao request) {
 		return calculadoraService.subtrair(request);
 	}
-	
-	@PostMapping("/calculadora/multiplicar")
+
+	@PostMapping("/multiplicar")
 	public CalculadoraResposta calculadoraMultiplicar(@RequestBody CalculadoraRequisicao request) {
 		return calculadoraService.multiplicar(request);
 	}
-	
-	@PostMapping("/calculadora/dividir")
+
+	@PostMapping("/dividir")
 	public CalculadoraResposta calculadoraDividir(@RequestBody CalculadoraRequisicao request) {
 		return calculadoraService.dividir(request);
-	}	
+	}
 }
